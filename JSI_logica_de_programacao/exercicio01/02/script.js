009e20465b17f4e377b2584ff6f381a58234f8ac
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-06-07 14:19:34 -0300

    JS I - Calculadora de 4 operações

---
+++ JSI_logica_de_programacao/exercicio01/02/script.js
@@ -0,0 +1,9 @@
+const numero01 = parseInt(prompt('Digite o primeiro número:'));
+const numero02 = parseInt(prompt('Digite o segundo número:'));
+
+alert(`
+Soma: ${numero01 + numero02}
+Subtração: ${numero01 - numero02}
+Multiplicação: ${numero01 * numero02}
+Divisão: ${numero01 / numero02}
+`);
